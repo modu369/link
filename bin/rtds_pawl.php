27b90cdb522b1d95ec36b290fc5e25cca785d13f
modu369 <ainet258369@gmail.com>
2025-12-25 01:04:48 -0600

    Fix Pawl connect import

--- bin/rtds_pawl.php
+++ bin/rtds_pawl.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Ratchet\Client\WebSocket;
-use Ratchet\Client\connect;
+use Ratchet\Client\connect as ws_connect;
 
 $options = getopt('', ['send::', 'help']);
 if (isset($options['help'])) {
@@ -13,7 +13,7 @@ if (isset($options['help'])) {
 
 $sendPayload = $options['send'] ?? null;
 
-connect('wss://ws-live-data.polymarket.com')->then(
+ws_connect('wss://ws-live-data.polymarket.com')->then(
     function (WebSocket $conn) use ($sendPayload) {
         echo "Connected\n";
 
