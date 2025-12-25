11c2e261caff30360057e6b8ca0736c607b236af
modu369 <ainet258369@gmail.com>
2025-12-25 00:58:06 -0600

    Add Pawl websocket sample

---
+++ bin/rtds_pawl.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Ratchet\Client\WebSocket;
+use Ratchet\Client\connect;
+
+$options = getopt('', ['send::', 'help']);
+if (isset($options['help'])) {
+    echo "Usage: php bin/rtds_pawl.php --send='{\"type\":\"subscribe\",\"channel\":\"markets\"}'\n";
+    exit(0);
+}
+
+$sendPayload = $options['send'] ?? null;
+
+connect('wss://ws-live-data.polymarket.com')->then(
+    function (WebSocket $conn) use ($sendPayload) {
+        echo "Connected\n";
+
+        if ($sendPayload !== null) {
+            $conn->send($sendPayload);
+        }
+
+        $conn->on('message', function ($msg) {
+            echo $msg . "\n";
+        });
+
+        $conn->on('close', function () {
+            echo "Connection closed\n";
+        });
+    },
+    function (\Exception $e) {
+        echo "Could not connect: {$e->getMessage()}\n";
+    }
+);
