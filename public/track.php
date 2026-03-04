bea56b741b123fc22a1496f1e965bef9fe79052c
modu369 <ainet258369@gmail.com>
2026-03-04 20:27:43 +0800

    Add files via upload

--- public/track.php
+++ public/track.php
@@ -60,10 +60,7 @@ $extractIp = static function (?string $value): ?string {
     return null;
 };
 
-$clientIp = $extractIp($_SERVER['HTTP_CF_CONNECTING_IP'] ?? null)
-    ?? $extractIp($_SERVER['HTTP_TRUE_CLIENT_IP'] ?? null)
-    ?? $extractIp($_SERVER['HTTP_X_REAL_IP'] ?? null)
-    ?? $extractIp($_SERVER['HTTP_X_FORWARDED_FOR'] ?? null)
+$clientIp = $extractIp($_SERVER['HTTP_X_REAL_IP'] ?? null)
     ?? $extractIp($_SERVER['REMOTE_ADDR'] ?? null);
 
 $userAgent = $sanitizeText($_SERVER['HTTP_USER_AGENT'] ?? '', 512);
