f150d682b4aa9efd999c4c49ddfca66561a109b7
modu369 <ainet258369@gmail.com>
2026-04-20 21:02:23 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -167,7 +167,7 @@ CREATE TABLE IF NOT EXISTS pageview_bot_logs (
     INDEX idx_bot_time (bucket_start),
     INDEX idx_bot_site_time (site_id, occurred_at)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
-/*!50100 PARTITION BY HASH (site_id */;
+/*!50100 PARTITION BY HASH (site_id */
 -- 新增普通用户表
 CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
